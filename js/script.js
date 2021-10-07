b69a39cab583713e3f0539eeeadff2be499be51a
heatfreshdeveloper <72042291+heatfreshdeveloper@users.noreply.github.com>
2021-10-07 17:00:59 +0200

    Aggiunti prompt e alert richiesti

---
+++ js/script.js
@@ -0,0 +1,6 @@
+
+const nomeUtente = prompt("Qual è il tuo nome?");
+const cognomeUtente = prompt("Qual è il tuo cognome?");
+const favColor = prompt("Qual è il tuo colore preferito?");
+
+alert(nomeUtente + cognomeUtente + favColor);
