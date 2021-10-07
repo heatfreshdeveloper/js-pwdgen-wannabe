7de7ec413dc1d7f0fac82ca92a43d8b26f4baccb
heatfreshdeveloper <72042291+heatfreshdeveloper@users.noreply.github.com>
2021-10-07 17:16:16 +0200

    Aggiunta la stringa a fine concatenazione

--- js/script.js
+++ js/script.js
@@ -3,4 +3,4 @@ const nomeUtente = prompt("Qual è il tuo nome?");
 const cognomeUtente = prompt("Qual è il tuo cognome?");
 const favColor = prompt("Qual è il tuo colore preferito?");
 
-alert(nomeUtente + cognomeUtente + favColor);
+alert(nomeUtente + cognomeUtente + favColor + "21");
